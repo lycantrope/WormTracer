6ce0fc3528adcf6a5eb660d30dd86a93e4af73b4
Chung-Kuan Chen <b97b01045@gmail.com>
2025-08-03 20:29:52 +0900

    Update losses

--- WormTracer20_1/WormTracer/functions.py
+++ WormTracer20_1/WormTracer/functions.py
@@ -1270,7 +1270,7 @@ def train3(
         center_loss_weight * ((model.cx - init_cx) ** 2 + (model.cy - init_cy) ** 2),
     ]
     for i in range(len(losses)):
-        losses[i] = losses[i].clone().detach().cpu().numpy()
+        losses[i] = float(losses[i].clone().detach().cpu().numpy())
     return losses
 
 
@@ -1301,19 +1301,19 @@ def get_shape_params(shape_params, params):
 
 
 def loss_compare(loss_pair):
-    im_select = int(max(loss_pair[0][0]) > max(loss_pair[1][0]))
-    con_select = int(max(loss_pair[0][1]) > max(loss_pair[1][1]))
-    smo_select = int(max(loss_pair[0][2]) > max(loss_pair[1][2]))
+    im_select = int(loss_pair[0][0] > loss_pair[1][0])
+    con_select = int(loss_pair[0][1] > loss_pair[1][1])
+    smo_select = int(loss_pair[0][2] > loss_pair[1][2])
     if im_select + con_select + smo_select == 3:
         return 1
     if im_select + con_select + smo_select == 0:
         return 0
     q75, q50, q25 = np.percentile(loss_pair[im_select][0], [75, 50, 25])
-    im_exrate = (max(loss_pair[1 - im_select][0]) - q50) / (q75 - q25)
+    im_exrate = (loss_pair[1 - im_select][0] - q50) / (q75 - q25)
     q75, q50, q25 = np.percentile(loss_pair[im_select][1], [75, 50, 25])
-    con_exrate = (max(loss_pair[1 - con_select][1]) - q50) / (q75 - q25)
+    con_exrate = (loss_pair[1 - con_select][1] - q50) / (q75 - q25)
     q75, q50, q25 = np.percentile(loss_pair[im_select][2], [75, 50, 25])
-    smo_exrate = (max(loss_pair[1 - smo_select][2]) - q50) / (q75 - q25)
+    smo_exrate = (loss_pair[1 - smo_select][2] - q50) / (q75 - q25)
     exrate_loss = np.argmax(np.array([im_exrate, con_exrate, smo_exrate]))
     return [im_select, con_select, smo_select][exrate_loss]
 
