49550dd4f8a02068813cdb9adc243c9a59163e36
Chung-Kuan Chen <b97b01045@gmail.com>
2026-03-25 19:12:00 +0900

    Update get_parser

--- src/WormTracer/__main__.py
+++ src/WormTracer/__main__.py
@@ -36,15 +36,91 @@ def get_parser():
         dest="guide_files",
         nargs="+",
         type=pathlib.Path,
-        help="Path to guided centerline. (*_x.csv, *_y.csv or *_skel.h5)",
+        help="Path to guided centerline. (*_x.csv, *_y.csv or *.h5)",
+    )
+
+    parser.add_argument("--start_T", type=int, help="Start frame of WormTracer")
+    parser.add_argument(
+        "--end_T",
+        type=int,
+        help="End frame of WormTracer (set to 0 process all frame)",
+    )
+
+    parser.add_argument("--rescale", type=float, help="Rescale of input images")
+
+    parser.add_argument("--Tscale", type=int)
+    parser.add_argument("--continuity_loss_weight", type=float)
+    parser.add_argument("--smoothness_loss_weight", type=float)
+    parser.add_argument("--length_loss_weight", type=float)
+    parser.add_argument("--center_loss_weight", type=float)
+    parser.add_argument("--plot_n", type=int)
+    parser.add_argument("--epoch_plus", type=int)
+    parser.add_argument("--speed", type=float)
+    parser.add_argument("--lr", type=float)
+    parser.add_argument("--body_ratio", type=float)
+    parser.add_argument("--judge_head_method", choices=["amplitude", "frequency"])
+
+    parser.add_argument(
+        "--num_t",
+        type=int,
+    )
+
+    parser.add_argument(
+        "--ShowProgress",
+        action="store_true",
+        help="If True, shows progress during optimization repeats.",
+        default=None,
+    )
+    parser.add_argument(
+        "--SaveProgress",
+        action="store_true",
+        help="If True, saves worm images during optimization in `progress_image` folder created in datafolder.",
+        default=None,
+    )
+
+    parser.add_argument(
+        "--show_progress_freq",
+        type=int,
+        help="This value is epoch frequency of displaying tracing progress.",
+    )
+    parser.add_argument(
+        "--save_progress_freq",
+        type=int,
+        help="This value is epoch frequency of saving tracing progress.",
+    )
+    parser.add_argument(
+        "--save_progress_num",
+        type=int,
+        help="This value is the number of images that are included in saved progress tracing.",
+    )
+
+    parser.add_argument(
+        "--SaveCenterlinedWormsSerial",
+        action="store_true",
+        help="If True, saves input images with estimated centerline as seirial numbered png files in full_line_images folder.",
+        default=None,
+    )
+    parser.add_argument(
+        "--SaveCenterlinedWormsMovie",
+        action="store_true",
+        help="If True, saves input images with estimated centerline as a movie full_line_images.mp4",
+        default=None,
+    )
+    parser.add_argument(
+        "--SaveCenterlinedWormsMultitiff",
+        action="store_true",
+        help="If True, saves input images with estimated centerline as a multipage tiff full_line_images.tif",
+        default=None,
     )
     return parser
 
 
 def main():
     parser = get_parser()
-    args, unknown_params = parser.parse_known_args()
-    wt.run(**vars(args))
+    args = parser.parse_args()
+    conf = {k: v for k, v in vars(args).items() if v is not None}
+
+    wt.run(**conf)
 
 
 def main_wrapper():
